69c965e47d95988388299e3b429b119d9a67bacb
Vinícius Figueiredo <viniciusfs@gmail.com>
2017-01-29 18:11:31 -0200

    Changed default NTP server to ntp.org servers :)

--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 
 Install and configure NTP client in Linux systems.
 
+
 ## Role Variables
 
-Name | Description | Default value
------|-------------|--------------
-`ntp_config_enabled` | Enable service at boot time | True
-`ntp_config_servers` | List of NTP servers to sync | `[ "a.ntp.br", "b.ntp.br", "c.ntp.br" ]`
-`ntp_config_timezone` | Server time zone | `America/Sao_Paulo`
+* `ntp_config_enabled`:
+    - Description: Enable service at boot time
+    - Values: `True | False`
+    - Default: `True`
+
+* `ntp_config_servers`:
+    - Description: List of NTP ervers to sync
+    - Default: `[ "0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org" ]`
+
+* `ntp_config_timezone`:
+    - Description: Server time zone
+    - Default: `America/Sao_Paulo`
 
 
 ## Example Playbook
--- tests/test_ntp.py
+++ tests/test_ntp.py
@@ -1,5 +1,6 @@
 import testinfra.utils.ansible_runner
 
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
@@ -14,9 +15,9 @@ def test_ntp_conf_file(File):
     assert ntp_conf.user == 'root'
     assert ntp_conf.group == 'root'
     assert ntp_conf.mode == 0664
-    assert ntp_conf.contains('server a.ntp.br iburst')
-    assert ntp_conf.contains('server b.ntp.br iburst')
-    assert ntp_conf.contains('server c.ntp.br iburst')
+    assert ntp_conf.contains('server 0.pool.ntp.org iburst')
+    assert ntp_conf.contains('server 1.pool.ntp.org iburst')
+    assert ntp_conf.contains('server 2.pool.ntp.org iburst')
 
 
 def test_step_tickers_file(File):
@@ -24,9 +25,9 @@ def test_step_tickers_file(File):
     assert step_tickers.user == 'root'
     assert step_tickers.group == 'root'
     assert step_tickers.mode == 0664
-    assert step_tickers.contains('a.ntp.br')
-    assert step_tickers.contains('b.ntp.br')
-    assert step_tickers.contains('c.ntp.br')
+    assert step_tickers.contains('0.pool.ntp.org')
+    assert step_tickers.contains('1.pool.ntp.org')
+    assert step_tickers.contains('2.pool.ntp.org')
 
 
 def test_localtime_file(File):
